6a40d4c577c7df1cbce6c128a8d00929959a58b2
dhwpdnr <ink1237@gmail.com>
2023-08-16 19:27:03 +0900

    [refactor] seperate data in members

---
+++ static/js/songinfo.js
@@ -0,0 +1,40 @@
+var songInfo = {
+  faraway: {
+    namme: "Far Away",
+    singer: "Lacuna",
+    image: "img/faraway.jpg",
+    info: "개쩌는 곡 @@@@@@",
+    members: [
+      {
+        th: "36th",
+        name: "윤순형",
+        position: "보컬",
+        insta: "s__nbro",
+      },
+      {
+        th: "28th",
+        name: "정동환",
+        position: "일렉 기타",
+        insta: "bigsky_142857",
+      },
+      {
+        th: "36th",
+        name: "김서준",
+        position: "일렉 기타",
+        insta: "",
+      },
+      {
+        th: "30th",
+        name: "조용완",
+        position: "베이스",
+        insta: "re_base",
+      },
+      {
+        th: "32nd",
+        name: "오제욱",
+        position: "드럼",
+        insta: "oook2",
+      },
+    ],
+  },
+};
--- static/js/songs.js
+++ static/js/songs.js
@@ -5,18 +5,21 @@ var teams = {
       singer: "Lacuna",
       info: "개쩌는 곡",
       image: "img/faraway.jpg",
+      nameId: "faraway",
     },
     {
       name: "Realize",
       singer: "SilCagel",
       info: "개쩌는 곡ddddd",
       image: "",
+      nameId: "faraway",
     },
     {
       name: "어느샌가 우리는 매일을 살아왔네",
       singer: "The Polse",
       info: "개쩌는 ddddddd 곡",
       image: "",
+      nameId: "faraway",
     },
   ],
   omniB: [
@@ -25,22 +28,28 @@ var teams = {
       singer: "TOUCHED",
       info: "개쩌는 곡",
       image: "",
+      nameId: "faraway",
     },
     {
       name: "ALIVE",
       singer: "TOUCHED",
       info: "개쩌는 곡ddddd",
       image: "",
+      nameId: "faraway",
     },
     {
       name: "좋지 아니한가",
       singer: "유다빈 밴드",
       info: "개쩌는 ddddddd 곡",
       image: "",
+      nameId: "faraway",
     },
   ],
 };
 
 function MoveTeamPage(teamName) {
   window.location.href = "omniA.html?team=" + teamName;
+}
+function MoveSongPage(songId) {
+  window.location.href = "jocianihangaTeam.html?song=" + songId;
 }
